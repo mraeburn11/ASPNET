6f0d8ee5d42598db1d49be819b8b51bbf3d21894
mraeb <mraeburn111@gmail.com>
2020-07-30 14:26:20 -0500

    finished

---
+++ ASPNET/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using ASPNET.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Testing.Models;
+
+namespace ASPNET
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+        void UpdateProduct(Product prod);
+        public void InsertProduct(Product productToInsert);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+        public void DeleteProduct(Product product);
+    }
+}
---
+++ ASPNET/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+
+
+namespace Testing.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+
+        
+
+    }
+}
+
